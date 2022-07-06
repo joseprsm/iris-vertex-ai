3b96a82e7d47751282bf78307d944d7d94916bca
José Medeiros <joseprsm@gmail.com>
2022-07-06 16:53:47 +0100

    add training script

---
+++ iris-vertex-ai/train.py
@@ -0,0 +1,40 @@
+import os
+import click
+import pickle
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+
+OUTPUT_PATH = 'outputs'
+MODEL_PATH = os.path.join(OUTPUT_PATH, 'model.pkl')
+ENCODER_PATH = os.path.join(OUTPUT_PATH, 'encoder.pkl')
+
+
+@click.command
+@click.option('--data-path', default='data/iris.csv')
+def train(data_path: str):
+    df = pd.read_csv(data_path, header=None)
+    label = df.pop(df.shape[1] - 1)
+
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(label)
+
+    # noinspection PyPep8Naming
+    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=.3)
+    ppl = make_pipeline(MinMaxScaler(), RandomForestClassifier())
+
+    # noinspection PyUnresolvedReferences
+    ppl.fit(X_train, y_train).predict(X_test)
+
+    with open(MODEL_PATH, 'wb') as f:
+        pickle.dump(ppl, f)
+
+    with open(ENCODER_PATH, 'wb') as f:
+        pickle.dump(encoder, f)
+
+
+if __name__ == '__main__':
+    train()
