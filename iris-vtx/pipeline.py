944c9101c86df4f2eac5a0a76232b27a3d65f790
José Medeiros <joseprsm@gmail.com>
2022-07-15 15:04:29 +0100

    update pipeline definition

--- iris-vtx/pipeline.py
+++ iris-vtx/pipeline.py
@@ -4,8 +4,6 @@ from kfp.v2 import compiler, dsl
 
 from kfp.components import load_component_from_file
 
-from google.cloud import aiplatform as aip
-
 project_id = os.environ.get('PROJECT_ID')
 pipeline_root_path = os.environ.get('PIPELINE_ROOT')
 
@@ -14,13 +12,18 @@ def get_component(name: str):
     return load_component_from_file(f'components/{name}/component.yaml')
 
 
+download_op = get_component('download')
+train_op = get_component('train')
+deploy_op = get_component('deploy')
+
+
 @dsl.pipeline(
     name="iris-pipeline",
     pipeline_root=pipeline_root_path)
 def pipeline():
-    download_op = get_component('download')()
-    train = get_component('train')(training_data=download_op.outputs['data'])
+    download_task = download_op()
+    train_task = train_op(training_data=download_task.outputs['data'])
+    deploy_task = deploy_op(model_uri=train_task.outputs['model'].uri)
 
 
-aip.init(project=project_id)
 compiler.Compiler().compile(pipeline_func=pipeline, package_path='pipeline.json')
